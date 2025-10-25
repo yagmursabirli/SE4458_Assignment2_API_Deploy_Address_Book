65824e021a5cd3af6b5c7c77abc9a4b0c13fae28
yagmursabirli <yagmursabirli35@gmail.com>
2025-10-25 19:25:30 +0300

    swagger düzenlemesi

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,31 @@
 //index.js
 import express from "express";
-import contactsRoutes from './routes/contacts.js';
+import contactsRoutes from "./routes/contacts.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //swagger imports
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use('/contacts', contactsRoutes);
+app.use("/contacts", contactsRoutes);
 
 //routers
 
 app.get("/", (req, res) => {
-res.send("Hello from home page. Swagger API docs: /api-docs");
+  res.send("Hello from home page. Swagger API docs: /api-docs");
 });
 
 app.listen(PORT, () => {
