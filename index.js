4ffd606aa8c9eb2be0a7af51d58b7f0893b08b09
yagmursabirli <yagmursabirli35@gmail.com>
2025-10-25 20:06:44 +0300

    swagger

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-//index.js
 import express from "express";
 import contactsRoutes from "./routes/contacts.js";
-
 import path from "path";
 import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-//swagger imports
-import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 
-const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const swaggerDocumentPath = path.join(__dirname, "swagger.yaml");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.use("/contacts", contactsRoutes);
 
-//routers
+app.get("/api-docs", (req, res) => {
+  res.redirect(
+    "https://petstore.swagger.io/?url=" +
+      req.protocol +
+      "://" +
+      req.get("host") +
+      "/swagger.yaml"
+  );
+});
+
+app.get("/swagger.yaml", (req, res) => {
+  res.sendFile(swaggerDocumentPath);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello from home page. Swagger API docs: /api-docs");
 });
 
 app.listen(PORT, () => {
-  console.log(`server running on port: http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
